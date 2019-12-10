fcb59209f1499b651924c0d395bc02b9d36abe3f
Anuj Kapil <anuj.kapil@corelogic.com.au>
2019-12-10 05:21:49 +0000

    updates

--- R/main.R
+++ R/main.R
@@ -1,11 +1,6 @@
 library(data.table)
 library(tidyverse)
 
-
-
-
-sample_n()
-
 zip1 <- 'https://drive.google.com/uc?id=0B3o2JsiUpwEvLTRDNkEyZmZZM1U&export=download'
 zip1csv <- data.table(read.table(unz(zip1, 'trip_fare_4.csv')))
 ?fread
@@ -20,38 +15,46 @@ str(bank)
 
 getwd()
 
-trip_fare[, .N, by = list(medallion, hack_license, vendor_id)]
-
 
 trip_fare <- fread('Data/trip_fare_4.csv')
 trip_data <- fread('Data/trip_data_4.csv')
 
+trip_fare[, .N]
+#15100468
+
+uniqueN(trip_fare, by = key(trip_fare))
+?uniqueN
+#15100414
+
+trip_data[,.N]
+#15100468
+
+uniqueN(trip_data)
+#15100459
+
+
+
+join_keycols <- names(trip_fare)[1:4]
+#td_keycols <- c(names(trip_data)[1:3], 'pickup_datetime')
+setkeyv(trip_fare, join_keycols)
+setkeyv(trip_data, join_keycols)
+
+trip_combined <- trip_fare[trip_data, nomatch = 0]
+trip_combined[, .N]
+#15101772
+
+uniqueN(trip_combined)
+#15101654
+
+?setkeyv
+trip_data[, sample_flg := sample(c(TRUE, FALSE), size = .N, replace = TRUE, prob = c(0.1, 0.9))]
+
+trip_data_sample <- trip_data[sample_flg == T]
+
+
 trip_fare
 hist(trip_data$passenger_count)
 
-trip_data_bkp <- copy(trip_data)
-
-
-trip_data[passenger_count < 8, sample_n(100), by = list(passenger_count)]
-
-
-
-trip_data[, .N, by = hour(pickup_datetime)]
-set.seed(131)
-
-
-trip_data <- trip_data %>%
-  mutate(hpick = hour(pickup_datetime)) %>%
-  group_by(hpick, vendor_id) %>%
-  sample_n(1000)
-
-
-trip_data <- trip_data %>%
-  filter(passenger_count < 8) %>%
-  group_by(passenger_count) %>%
-  sample_n(100)
-
-setDT(trip_data)
 
 p1 <- trip_data %>%
   group_by(passenger_count) %>%
@@ -62,6 +65,17 @@ p1 <- trip_data %>%
   theme(legend.position = "none")
 
 p1
+
+
+p1_sample <- trip_data_sample %>%
+  group_by(passenger_count) %>%
+  count() %>%
+  ggplot(aes(passenger_count, n, fill = passenger_count)) +
+  geom_col() +
+  scale_y_sqrt() +
+  theme(legend.position = "none")
+
+p1_sample
 
 summary(trip_data)
 ggplot(data=trip_data, aes(x=passenger_count)) + 
@@ -87,6 +101,18 @@ p4 <- trip_data %>%
 
 p4
 
+p4_sample <- trip_data_sample %>%
+  mutate(wday = wday(pickup_datetime, label = TRUE)) %>%
+  group_by(wday, vendor_id) %>%
+  count() %>%
+  ggplot(aes(wday, n, colour = vendor_id)) +
+  geom_point(size = 4) +
+  labs(x = "Day of the week", y = "Total number of pickups") +
+  theme(legend.position = "none")
+
+p4_sample
+
+
 
 p5 <- trip_data %>%
   mutate(hpick = hour(pickup_datetime)) %>%
@@ -98,6 +124,17 @@ p5 <- trip_data %>%
   theme(legend.position = "none")
 
 p5
+
+p5_sample <- trip_data_sample %>%
+  mutate(hpick = hour(pickup_datetime)) %>%
+  group_by(hpick, vendor_id) %>%
+  count() %>%
+  ggplot(aes(hpick, n, color = vendor_id)) +
+  geom_point(size = 4) +
+  labs(x = "Hour of the day", y = "Total number of pickups") +
+  theme(legend.position = "none")
+
+p5_sample
 
 
 p6 <- trip_data %>%
@@ -112,5 +149,20 @@ p6 <- trip_data %>%
 
 p6
 
+p6_sample <- trip_data_sample %>%
+  mutate(hpick = hour(pickup_datetime)) %>%
+  group_by(hpick) %>%
+  count() %>%
+  ggplot(aes(reorder(hpick, -n), n, fill = reorder(hpick, -n))) +
+  geom_col() +
+  scale_y_sqrt() +
+  labs(x = "Hour of the day", y = "Total number of pickups") +
+  theme(legend.position = "none")
+
+p6_sample
+
+
 trip_data
   
+
+
