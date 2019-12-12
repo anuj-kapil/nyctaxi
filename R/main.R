2944a9701ca98851ff0e3f9491513670991290fb
anuj-kapil <anuj.kapil@gmail.com>
2019-12-13 10:46:20 +1100

    Updates

--- R/main.R
+++ R/main.R
@@ -1,27 +1,30 @@
 library(data.table)
 library(tidyverse)
 library(lubridate)
-zip1 <- 'https://drive.google.com/uc?id=0B3o2JsiUpwEvLTRDNkEyZmZZM1U&export=download'
-zip1csv <- data.table(read.table(unz(zip1, 'trip_fare_4.csv')))
-?fread
+# zip1 <- 'https://drive.google.com/uc?id=0B3o2JsiUpwEvLTRDNkEyZmZZM1U&export=download'
+# zip1csv <- data.table(read.table(unz(zip1, 'trip_fare_4.csv')))
+# ?fread
+# 
+# url_csv_gz <- 'http://github.com/anuj-kapil/nyctaxi/blob/master/Data/trip_combined_sample.csv.gz?raw=true'
+# 
+# con <- gzcon(url(url_csv_gz))
+# txt <- readLines(con)
+# dat <- read.csv(textConnection(txt))
+# 
+# 
+# temp <- tempfile(fileext = ".gz")
+# download.file(url_csv_gz, temp)
+# out <- unzip(temp, exdir = tempdir())
+# 
+# bank <- read.csv(out[14], sep = ";")
+# str(bank)
+# 
+# getwd()
+# 
+# gc()
 
-url_csv_gz <- 'http://github.com/anuj-kapil/nyctaxi/blob/master/Data/trip_combined_sample.csv.gz?raw=true'
-
-con <- gzcon(url(url_csv_gz))
-txt <- readLines(con)
-dat <- read.csv(textConnection(txt))
-
-
-temp <- tempfile(fileext = ".gz")
-download.file(url_csv_gz, temp)
-out <- unzip(temp, exdir = tempdir())
-
-bank <- read.csv(out[14], sep = ";")
-str(bank)
-
-getwd()
-
-gc()
+###########################################
+# Prepare sample dataset
 
 trip_fare <- fread('Data/trip_fare_4.csv')
 trip_data <- fread('Data/trip_data_4.csv')
@@ -66,10 +69,11 @@ trip_combined[, sample_flg := sample(c(TRUE, FALSE), size = .N, replace = TRUE, 
 trip_combined_sample <- trip_combined[sample_flg == T]
 
 fwrite(trip_combined_sample, 'Data/trip_combined_sample.csv')
+###########################################
 
-trip_fare
+trip_combined_sample <- fread('Data/trip_combined_sample.csv.gz')
+
 hist(trip_combined_sample$passenger_count)
-
 
 # p1 <- trip_data %>%
 #   group_by(passenger_count) %>%
@@ -100,7 +104,7 @@ p1_sample
 #                  alpha = .2) + 
 #   labs(title="Histogram for Passenger Count", x="Passenger Count", y="Count") + 
 #   xlim(c(-1,10)) 
- #+ 
+#+ 
 #  ylim(c(0,30))
 
 # 
@@ -142,8 +146,8 @@ p3_filtered_sample <- trip_combined_sample %>%
   filter(fare_amount<=75) %>%
   ggplot(aes(fare_amount)) +
   geom_histogram(binwidth = 5) +
-#scale_y_sqrt() +
-theme(legend.position = "none")
+  #scale_y_sqrt() +
+  theme(legend.position = "none")
 
 p3_filtered_sample
 
@@ -189,7 +193,49 @@ p5_filtered_sample <- trip_combined_sample %>%
 p5_filtered_sample
 
 summary(trip_combined_sample$trip_time_in_secs)
-trip_combined_sample[, sd(trip_time_in_secs)]
+
+hist(trip_combined_sample$trip_time_in_secs)
+
+
+ggplot(data=trip_combined_sample, aes(x=trip_time_in_secs)) +
+  geom_histogram(breaks=seq(0, 10, by=1),
+                 col="red",
+                 fill="green",
+                 alpha = .2) +
+  labs(title="Histogram for Passenger Count", x="Passenger Count", y="Count") +
+  xlim(c(-1,10))
+
+10000/60/60
+
+
+plot_sd <- trip_combined_sample[, sd(trip_time_in_secs), by = cut_interval(trip_time_in_secs/60/60, 12)]
+
+ggplot(data=plot_sd, aes(x=cut_interval, y = V1, fill = cut_interval)) +
+  geom_col() +
+  labs(title="Histogram", x="Time interval", y="Count")
+
+trip_combined_sample[trip_time_in_secs <= 15*60, sd(trip_time_in_secs)]
+
+trip_combined_sample[trip_time_in_secs <= 30*60, sd(trip_time_in_secs)]
+
+trip_combined_sample[trip_time_in_secs > 30*60, sd(trip_time_in_secs)]
+
+trip_combined_sample[trip_time_in_secs > 4*30*60, sd(trip_time_in_secs)]
+
+
+
+plot_sd_fare <- trip_combined_sample[, sd(total_amount), by = cut_interval(total_amount, 20)]
+
+ggplot(data=plot_sd_fare, aes(x=cut_interval, y = V1, fill = cut_interval)) +
+  geom_col() +
+  labs(title="Histogram", x="Fare interval", y="Count")
+
+
+
+
+
+
+
 
 p4_sample <- trip_combined_sample %>%
   mutate(wday = wday(pickup_datetime, label = TRUE)) %>%
@@ -254,10 +300,20 @@ p6_sample
 
 trip_data
 
-hist(trip_combined_sample$pickup_longitude)
 
-trip_combined_sample <- fread('Data/trip_combined_sample.csv.gz')
 trip_combined_sample[, .N]
+
+hist(trip_combined_sample$pickup_latitude)
+
+
+ggplot(head(trip_combined_sample, 1000), aes(x=pickup_longitude, y= pickup_latitude), size=2200) +
+  geom_point()
+
+
+library(plotly)
+
+
+#plot_ly(data = trip_combined_sample, x = ~pickup_longitude, y = ~pickup_latitude)
 
 # oultier analysis
 
@@ -272,7 +328,7 @@ trip_combined_sample <- trip_combined_sample[pickup_latitude %between% c(30, 50)
 ggplot(trip_combined_sample, aes(x="pickup_longitude", y=pickup_longitude))+geom_boxplot()
 
 boxplot.stats(trip_combined_sample$pickup_longitude)
-  ggplot(trip_combined_sample, aes(x=pickup_latitude, y=pickup_longitude))+geom_point()
+ggplot(trip_combined_sample, aes(x=pickup_latitude, y=pickup_longitude))+geom_point()
 
 summary(trip_combined_sample)
 
