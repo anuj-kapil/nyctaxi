37281c70b23a9c3bf533a8dede19256d3aa626d7
Anuj Kapil <anuj.kapil@corelogic.com.au>
2019-12-16 07:26:41 +0000

    Updating commentary

--- R/main.R
+++ R/main.R
@@ -6,7 +6,8 @@ list.of.packages <- c(
   "lubridate",
   "plotly",
   "leaflet",
-  "geosphere"
+  "geosphere",
+  "ggthemes"
 )
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
@@ -19,6 +20,7 @@ library(lubridate)
 library(plotly)
 library(leaflet)
 library(geosphere)
+library(ggthemes)
 
 ###########################################
 # Prepare sample dataset
@@ -101,22 +103,79 @@ summary(trip_combined_sample$dropoff_longitude)
 
 # outlier analysis
 
-hist(trip_combined_sample$pickup_latitude)
-ggplot(trip_combined_sample, aes(x="pickup_latitude", y=pickup_latitude))+geom_boxplot()
+hist(trip_combined_sample$pickup_latitude,
+      main="Histogram for Pickup Latitude", 
+      xlab="Pickup Latitude", 
+      border="black", 
+      col="lightblue")
+
+ggplot(trip_combined_sample, aes(x="pickup_latitude", y=pickup_latitude)) +
+  geom_boxplot() +
+  labs(x = "", y = "")+
+  ggtitle("Box Plot for Pickup Latitude") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) +
+  scale_colour_tableau()
 
 # We know that NYC is in northern hemisphere. Some of the pickups are from equator (latitude = 0) and some are from southern hemisphere
 # Clearly data is wrongly captured or there were errors in capturing the data
 
-hist(trip_combined_sample$pickup_longitude)
-ggplot(trip_combined_sample, aes(x="pickup_longitude", y=pickup_longitude))+geom_boxplot()
+hist(trip_combined_sample$pickup_longitude,
+     main="Histogram for Pickup Longitude", 
+     xlab="Pickup Longitude", 
+     border="black", 
+     col="lightblue")
+
+ggplot(trip_combined_sample, aes(x="pickup_longitude", y=pickup_longitude)) +
+  geom_boxplot() +
+  labs(x = "", y = "")+
+  ggtitle("Box Plot for Pickup Longitude") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) +
+  scale_colour_tableau()
+
+hist(trip_combined_sample$dropoff_latitude,
+     main="Histogram for Dropoff Latitude", 
+     xlab="Dropoff Latitude", 
+     border="black", 
+     col="lightblue")
+
+ggplot(trip_combined_sample, aes(x="dropoff_latitude", y=dropoff_latitude)) +
+  geom_boxplot() +
+  labs(x = "", y = "")+
+  ggtitle("Box Plot for Dropoff Latitude") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) +
+  scale_colour_tableau()
 
 
-hist(trip_combined_sample$dropoff_latitude)
-ggplot(trip_combined_sample, aes(x="dropoff_latitude", y=dropoff_latitude))+geom_boxplot()
+hist(trip_combined_sample$dropoff_longitude,
+     main="Histogram for Dropoff Longitude", 
+     xlab="Dropoff Longitude", 
+     border="black", 
+     col="lightblue")
 
-hist(trip_combined_sample$dropoff_longitude)
-ggplot(trip_combined_sample, aes(x="dropoff_longitude", y=dropoff_longitude))+geom_boxplot()
-
+ggplot(trip_combined_sample, aes(x="dropoff_longitude", y=dropoff_longitude)) +
+  geom_boxplot() +
+  labs(x = "", y = "")+
+  ggtitle("Box Plot for Dropoff Longitude") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank()) +
+  scale_colour_tableau()
 
 # NYC range within 1000 kms radius (1 degree equal appox 111 kms) and NYC coordinates are 40.7141667 lat and -74.0063889 long
 trip_combined_sample <- trip_combined_sample[pickup_latitude %between% c(30, 50) & pickup_longitude %between% c(-84, -64)]
@@ -140,12 +199,20 @@ leaflet(data = head(trip_combined_sample, 1000)) %>%
 
 
 # Remove zero distance trips
-
 summary(trip_combined_sample)
 trip_combined_sample <- trip_combined_sample[trip_distance > 0]
 
 # Remove zero time trips
 trip_combined_sample <- trip_combined_sample[trip_time_in_secs > 0]
+
+summary(trip_combined_sample$trip_time_in_secs)
+
+trip_combined_sample[trip_time_in_secs>1849.257][order(-trip_time_in_secs)]
+
+ggplot(trip_combined_sample, aes(x="trip_time_in_secs", y=trip_time_in_secs))+geom_boxplot()
+
+trip_combined_sample[,.(min(trip_time_in_secs), max(trip_time_in_secs),mean(trip_time_in_secs) + 2 * sd(trip_time_in_secs),  mean(trip_time_in_secs) - 2 * sd(trip_time_in_secs))]
+
 
 hist(trip_combined_sample$trip_time_in_secs)
 
@@ -153,207 +220,211 @@ hist(trip_combined_sample$fare_amount)
 
 trip_combined_sample[fare_amount>=500]
 
-trip_combined_sample$passenger_count <- as.factor(trip_combined_sample$passenger_count)
-p1_sample <- trip_combined_sample %>%
-  group_by(passenger_count) %>%
-  count() %>%
-  ggplot(aes(passenger_count, n, fill = passenger_count)) +
+
+# Univariate Analysis
+
+plot_passenger_dist <- trip_combined_sample[, .N, by = list(passenger_count)]
+
+plot_passenger_dist$passenger_count <- as.factor(plot_passenger_dist$passenger_count)
+ggplot(plot_passenger_dist, aes(passenger_count, N, fill = passenger_count)) +
   geom_col() +
   scale_y_sqrt() +
-  theme(legend.position = "none")
+  labs(x = "Passenger Count", y = "Number of Trips")+
+  ggtitle("Histogram of Passenger Count") +
+  theme_bw() +
+  theme(panel.border = element_blank(),  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  scale_colour_tableau()
 
-p1_sample
+# Count trips group by payment type
+plot_payment_type_dist <- trip_combined_sample[, .N, by = list(payment_type)]
 
+# Convert the payment type to a categorical variable
+plot_payment_type_dist$payment_type <- as.factor(plot_payment_type_dist$payment_type)
 
-
-trip_combined_sample$payment_type <- as.factor(trip_combined_sample$payment_type)
-p2_sample <- trip_combined_sample %>%
-  group_by(payment_type) %>%
-  count() %>%
-  ggplot(aes(payment_type, n, fill = payment_type)) +
+# Visualize
+ggplot(plot_payment_type_dist, aes(payment_type, N, fill = payment_type)) +
   geom_col() +
   scale_y_sqrt() +
-  theme(legend.position = "none")
+  labs(x = "Payment Type", y = "Number of Trips")+
+  ggtitle("Histogram of Payment Type") +
+  theme_bw() +
+  theme(panel.border = element_blank(),  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  scale_colour_tableau()
 
-p2_sample
-
-
-summary(trip_combined_sample)
-hist(trip_combined_sample$fare_amount)
-p3_sample <- trip_combined_sample %>%
-  ggplot(aes(fare_amount)) +
-  geom_histogram(binwidth = 20) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
-
-p3_sample
-
-p3_filtered_sample <- trip_combined_sample %>%
-  filter(fare_amount<=75) %>%
-  ggplot(aes(fare_amount)) +
-  geom_histogram(binwidth = 5) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
-
-p3_filtered_sample
-
+### Distribution of Fare Amount
+ggplot(trip_combined_sample, aes(fare_amount)) +
+  geom_histogram(binwidth = 20,
+                 col="black", 
+                 fill="light blue") +
+  labs(x = "Fare Amount", y = "Number of Trips")+
+  ggtitle("Histogram of Fare Amount") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  scale_colour_tableau()
 
 hist(trip_combined_sample$tip_amount)
 
+### Distribution of Tip Amount
 
-p4_sample <- trip_combined_sample %>%
-  ggplot(aes(tip_amount)) +
-  geom_histogram(binwidth = 10) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
-
-p4_sample
-
-p4_filtered_sample <- trip_combined_sample %>%
-  filter(tip_amount<=20) %>%
-  ggplot(aes(tip_amount)) +
-  geom_histogram(binwidth = 2.5) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
-
-p4_filtered_sample
-
+ggplot(trip_combined_sample, aes(tip_amount)) +
+  geom_histogram(binwidth = 10,
+                 col="black", 
+                 fill="light blue") +
+  labs(x = "Tip Amount", y = "Number of Trips")+
+  ggtitle("Histogram of Tip Amount") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  scale_colour_tableau()
 
 
 hist(trip_combined_sample$total_amount)
-p5_sample <- trip_combined_sample %>%
-  ggplot(aes(total_amount)) +
-  geom_histogram(binwidth = 20) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
 
-p5_sample
+ggplot(trip_combined_sample, aes(total_amount)) +
+  geom_histogram(binwidth = 20,
+                 col="black", 
+                 fill="light blue") +
+  labs(x = "Total Amount", y = "Number of Trips")+
+  ggtitle("Histogram of Total Amount") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "none") + 
+  scale_colour_tableau()
 
-p5_filtered_sample <- trip_combined_sample %>%
-  filter(total_amount<=75) %>%
-  ggplot(aes(total_amount)) +
-  geom_histogram(binwidth = 5) +
-  #scale_y_sqrt() +
-  theme(legend.position = "none")
+# summary(trip_combined_sample$trip_time_in_secs)
+# 
+# hist(trip_combined_sample$trip_time_in_secs)
+# 
+# 
+# ggplot(data=trip_combined_sample, aes(x=trip_time_in_secs)) +
+#   geom_histogram(breaks=seq(0, 10000, by=500),
+#                  col="red",
+#                  fill="green",
+#                  alpha = .2) +
+#   labs(title="Histogram for Passenger Count", x="Passenger Count", y="Count") 
+# 
+# 10000/60/60
+# 
+# 
+# plot_sd <- trip_combined_sample[, sd(trip_time_in_secs), by = cut_interval(trip_time_in_secs/60/60, 12)]
+# 
+# ggplot(data=plot_sd, aes(x=cut_interval, y = V1, fill = cut_interval)) +
+#   geom_col() +
+#   labs(title="Histogram", x="Time interval", y="Count")
+# 
+# trip_combined_sample[trip_time_in_secs <= 15*60, sd(trip_time_in_secs)]
+# 
+# trip_combined_sample[trip_time_in_secs <= 30*60, sd(trip_time_in_secs)]
+# 
+# trip_combined_sample[trip_time_in_secs > 30*60, sd(trip_time_in_secs)]
+# 
+# trip_combined_sample[trip_time_in_secs > 4*30*60, sd(trip_time_in_secs)]
+# 
+# 
+# 
+# plot_sd_fare <- trip_combined_sample[, sd(total_amount), by = cut_interval(total_amount, 20)]
+# 
+# ggplot(data=plot_sd_fare, aes(x=cut_interval, y = V1, fill = cut_interval)) +
+#   geom_col() +
+#   labs(title="Histogram", x="Fare interval", y="Count")
+# 
 
-p5_filtered_sample
+# Feature Engineering
 
-summary(trip_combined_sample$trip_time_in_secs)
+trip_combined_sample[, fare_per_dist := (fare_amount/trip_distance)]
 
-hist(trip_combined_sample$trip_time_in_secs)
+# Calculate 2 standard deviations
+trip_combined_sample[,.(min_fare = min(fare_per_dist), 
+                        max_fare = max(fare_per_dist),
+                        sd_2_fare_upper = mean(fare_per_dist) + 2 * sd(fare_per_dist), 
+                        sd_2_fare_lower = mean(fare_per_dist) - 2 * sd(fare_per_dist)
+)
+]
 
+# Visualize if the trip duration has an impact on the high fare_per_dist for lower trip distances
 
-ggplot(data=trip_combined_sample, aes(x=trip_time_in_secs)) +
-  geom_histogram(breaks=seq(0, 10, by=1),
-                 col="red",
-                 fill="green",
-                 alpha = .2) +
-  labs(title="Histogram for Passenger Count", x="Passenger Count", y="Count") +
-  xlim(c(-1,10))
-
-10000/60/60
-
-
-plot_sd <- trip_combined_sample[, sd(trip_time_in_secs), by = cut_interval(trip_time_in_secs/60/60, 12)]
-
-ggplot(data=plot_sd, aes(x=cut_interval, y = V1, fill = cut_interval)) +
-  geom_col() +
-  labs(title="Histogram", x="Time interval", y="Count")
-
-trip_combined_sample[trip_time_in_secs <= 15*60, sd(trip_time_in_secs)]
-
-trip_combined_sample[trip_time_in_secs <= 30*60, sd(trip_time_in_secs)]
-
-trip_combined_sample[trip_time_in_secs > 30*60, sd(trip_time_in_secs)]
-
-trip_combined_sample[trip_time_in_secs > 4*30*60, sd(trip_time_in_secs)]
+ggplot(head(trip_combined_sample, 10000)) + 
+  geom_point(aes(x=trip_distance, y=fare_per_dist, col = (trip_time_in_secs/60))) + 
+  labs(x = "Trip Distance", y = "Fare amount per unit distance") +
+  labs(col = "Trip Time (in mins)") +
+  ggtitle("Fare amount per unit distance Vs trip distance & time") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))
 
 
+trip_combined_sample <- trip_combined_sample[fare_per_dist < (mean(fare_per_dist) + 2 * sd(fare_per_dist))]
 
-plot_sd_fare <- trip_combined_sample[, sd(total_amount), by = cut_interval(total_amount, 20)]
+# Weekday
+trip_combined_sample[, pickup_wday := wday(pickup_datetime, label = TRUE)]
 
-ggplot(data=plot_sd_fare, aes(x=cut_interval, y = V1, fill = cut_interval)) +
-  geom_col() +
-  labs(title="Histogram", x="Fare interval", y="Count")
+plot_wday_trips <- trip_combined_sample[, .N, by = list(pickup_wday, vendor_id)]
 
-
-
-
-
-
-
-
-p4_sample <- trip_combined_sample %>%
-  mutate(wday = wday(pickup_datetime, label = TRUE)) %>%
-  group_by(wday, vendor_id) %>%
-  count() %>%
-  ggplot(aes(wday, n, colour = vendor_id)) +
+ggplot(plot_wday_trips, aes(pickup_wday, N, colour = vendor_id)) +
   geom_point(size = 4) +
   labs(x = "Day of the week", y = "Total number of pickups") +
-  theme(legend.position = "none")
-
-p4_sample
-
-
-
-p5 <- trip_data %>%
-  mutate(hpick = hour(pickup_datetime)) %>%
-  group_by(hpick, vendor_id) %>%
-  count() %>%
-  ggplot(aes(hpick, n, color = vendor_id)) +
-  geom_point(size = 4) +
-  labs(x = "Hour of the day", y = "Total number of pickups") +
-  theme(legend.position = "none")
-
-p5
-
-p5_sample <- trip_combined_sample %>%
-  mutate(hpick = hour(pickup_datetime)) %>%
-  group_by(hpick, vendor_id) %>%
-  count() %>%
-  ggplot(aes(hpick, n, color = vendor_id)) +
-  geom_point(size = 4) +
-  labs(x = "Hour of the day", y = "Total number of pickups") +
-  theme(legend.position = "none")
-
-p5_sample
+  labs(col = "Vendor") +
+  ggtitle("Taxi trips by day of the week") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5))
 
 
-p6 <- trip_data %>%
-  mutate(hpick = hour(pickup_datetime)) %>%
-  group_by(hpick) %>%
-  count() %>%
-  ggplot(aes(reorder(hpick, -n), n, fill = reorder(hpick, -n))) +
+# Hour of the day
+
+trip_combined_sample[, pickup_hour := hour(pickup_datetime)]
+
+plot_hourly_trips <- trip_combined_sample[, .N, by = list(pickup_hour)]
+
+ggplot(plot_hourly_trips, aes(reorder(pickup_hour, -N), N, fill = reorder(pickup_hour, -N))) +
   geom_col() +
   scale_y_sqrt() +
   labs(x = "Hour of the day", y = "Total number of pickups") +
+  ggtitle("Taxi trips by hour of the day") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5)) +
   theme(legend.position = "none")
 
-p6
 
-p6_sample <- trip_combined_sample %>%
-  mutate(hpick = hour(pickup_datetime)) %>%
-  group_by(hpick) %>%
-  count() %>%
-  ggplot(aes(reorder(hpick, -n), n, fill = reorder(hpick, -n))) +
+plot_hourly_fares <- trip_combined_sample[, .(mean_hourly_fare = mean(fare_amount)), by = list(pickup_hour)]
+
+ggplot(plot_hourly_fares, aes(reorder(pickup_hour, -mean_hourly_fare), mean_hourly_fare, fill = reorder(pickup_hour, -mean_hourly_fare))) +
   geom_col() +
   scale_y_sqrt() +
-  labs(x = "Hour of the day", y = "Total number of pickups") +
+  labs(x = "Hour of the day", y = "Average Fare Amount") +
+  ggtitle("Fare amount by hour of the day") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5)) +
   theme(legend.position = "none")
 
-p6_sample
+plot_hourly_trip_duration <- trip_combined_sample[, .(mean_hourly_trip_duration = mean(trip_time_in_secs)), by = list(pickup_hour)]
 
-
+ggplot(plot_hourly_trip_duration, aes(reorder(pickup_hour, -mean_hourly_trip_duration), mean_hourly_trip_duration, fill = reorder(pickup_hour, -mean_hourly_trip_duration))) +
+  geom_col() +
+  scale_y_sqrt() +
+  labs(x = "Hour of the day", y = "Average Trip Duration") +
+  ggtitle("Trip Duration by hour of the day") +
+  theme(panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"), 
+        plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "none")
 
 # Mean Fare amount and mean distance
-trip_combined_sample[ , .(mean(fare_amount), mean(trip_distance)), by = list(passenger_count)]
+trip_combined_sample[ , .(mean(fare_amount), mean(trip_time_in_secs)), by = list(pickup_hour)]
 
 # Mean fare amount per unit distance
 trip_combined_sample[ , .(sum(fare_amount)/sum(trip_distance)), by = list(passenger_count)]
 
-
 # Plotting fare against distance
-ggplot(head(trip_combined_sample, 1000), aes(x=fare_amount, y=trip_distance))+geom_point()
+ggplot(head(trip_combined_sample, 10000), aes(x=fare_amount, y=trip_distance))+geom_point()
 
 # Linear relationship
 
@@ -370,12 +441,20 @@ hist(trip_combined_sample[round(pickup_latitude,2) == 40.64 & round(pickup_longi
 # hist where drop-off JFK
 hist(trip_combined_sample[round(dropoff_latitude,2) == 40.64 & round(dropoff_longitude,2) == -73.78]$fare_amount)
 
+trip_combined_sample[, fare_per_dist := NULL]
+trip_combined_sample[, fare_per_dist := (fare_amount/trip_distance)]
 
-trip_combined_sample[, fare_per_dist := fare_amount/trip_distance]
+trip_combined_sample[,.(min(fare_per_dist), max(fare_per_dist),mean(fare_per_dist) + 4 * sd(fare_per_dist),  mean(fare_per_dist) - 4 * sd(fare_per_dist))]
 
+trip_combined_sample[fare_per_dist > 48][order(-fare_per_dist)]
+
+trip_combined_sample[fare_amount >= 500]
 
 trip_combined_sample[fare_per_dist == 250]
-ggplot(head(trip_combined_sample, 10000), aes(x=trip_distance, y=fare_per_dist))+geom_point()
+
+trip_combined_sample$trip_time_in_secs
+ggplot(head(trip_combined_sample, 10000), aes(x=trip_distance, y=fare_per_dist, col = (trip_time_in_secs/60)))+geom_point()
+
 
 summary(trip_combined_sample)
 
@@ -390,7 +469,7 @@ plot_mean_time <- trip_combined_sample[, .(mean_time = mean(trip_time_in_secs)) 
 ggplot(plot_mean_time, aes(x=hour, y=mean_time))+geom_line(color = 'red')
 
 
-plot_mean_time_fare <- trip_combined_sample[, .(mean_fare_per_dist = mean(fare_per_dist), mean_time = mean(trip_time_in_secs/60)) , by = hour(pickup_datetime_dt)]
+plot_mean_time_fare <- trip_combined_sample[, .(mean_fare_per_dist = mean(fare_per_dist), mean_time = mean(trip_time_in_secs/60)) , by = hour(pickup_datetime)]
 
 ggplot(plot_mean_time_fare) +
   geom_line(aes(x=hour, y=mean_fare_per_dist), color = 'red') + 
